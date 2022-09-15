695920eb102085bd57848354f9a0afd67ede355d
Hee-Cheol Kim <41623476+dogfoots@users.noreply.github.com>
2022-09-15 18:25:02 +0900

    1
    1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 function openGnbMenu(){
-    $(".header_gnb_layer").fadeIn('500', function(){
+    $(".header_gnb_layer").fadeIn(150, function(){
         //$(".mainmenu li.active").addClass("lined");    
     });
     $('body').css('overflow','hidden');
@@ -7,7 +7,7 @@ function openGnbMenu(){
 
 
 function closeGnbMenu(){
-    $(".header_gnb_layer").fadeOut('500');
+    $(".header_gnb_layer").fadeOut(150);
     $('body').css('overflow','auto')
 }
 
@@ -22,8 +22,8 @@ function readyGnbSubmenu(submenuId){
     $(".header_gnb_layer .submenu .submenu-contents").hide();
     $("#"+submenuId).show();
 
-    $(".header_gnb_layer .mainmenu").fadeOut('250', function(){
-        $(".header_gnb_layer .submenu").fadeIn('250');
+    $(".header_gnb_layer .mainmenu").fadeOut(150, function(){
+        $(".header_gnb_layer .submenu").fadeIn(150);
         $(".submenu li.active").addClass("lined");    
     });   
     
@@ -31,8 +31,8 @@ function readyGnbSubmenu(submenuId){
 }
 
 function backToMainmenu(){
-    $(".header_gnb_layer .submenu").fadeOut('250',function(){
-        $(".header_gnb_layer .mainmenu").fadeIn('250');
+    $(".header_gnb_layer .submenu").fadeOut(150,function(){
+        $(".header_gnb_layer .mainmenu").fadeIn(150);
     });
     $(".submenu li").removeClass("lined");
 }
