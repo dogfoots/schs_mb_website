8d34358a5a07fc9b664963e28cf48995f01ab84c
Hee-Cheol Kim <41623476+dogfoots@users.noreply.github.com>
2022-09-15 13:57:54 +0900

    1
    1

--- js/common.js
+++ js/common.js
@@ -8,3 +8,21 @@ function closeGnbMenu(){
     $(".header_gnb_layer").fadeOut('500');
     $('body').css('overflow','auto')
 }
+
+
+function goToSubmenu(ele){
+    //console.log("ee : " + $(ele).parent().data("submenu-id"));
+    let submenu_id = $(ele).parent().data("submenu-id");
+    $(".header_gnb_layer .submenu .submenu-contents").hide();
+    $("#"+submenu_id).show();
+
+    $(".header_gnb_layer .mainmenu").fadeOut('250', function(){
+        $(".header_gnb_layer .submenu").fadeIn('250');
+    });
+}
+
+function backToMainmenu(){
+    $(".header_gnb_layer .submenu").fadeOut('250',function(){
+        $(".header_gnb_layer .mainmenu").fadeIn('250');
+    });
+}
